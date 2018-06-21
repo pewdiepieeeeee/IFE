839f7b58994fd892714349f7cbf42d4baaf764f9
pewdiepie <35588931+pewdiepieeeeee@users.noreply.github.com>
2018-06-22 02:36:41 +0800

    Update app.js

--- day37-38/JS/app.js
+++ day37-38/JS/app.js
@@ -2,3 +2,6 @@ createCheckbox('region-radio-wrapper',sourceData);
 createCheckbox('product-radio-wrapper',sourceData);
 document.getElementById('region-radio-wrapper').onchange=createTable;
 document.getElementById('product-radio-wrapper').onchange=createTable;
+var arry=[];
+arry=[];
+localStorage.setItem("dataArr1",JSON.stringify(arry));
